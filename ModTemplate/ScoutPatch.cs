4acdace4055653888f02226364b9bc3cf9a8e5eb
Vesper <0ooechooo0oooo@gmail.com>
2021-11-12 23:22:12 +0000

    Changed it so stream is always on, and you can snapshot

--- ModTemplate/ScoutPatch.cs
+++ ModTemplate/ScoutPatch.cs
@@ -1,9 +1,11 @@
 ﻿using OWML.ModHelper;
 using OWML.Common;
-using OWML.ModHelper.Events;
+
 using HarmonyLib;
 using UnityEngine;
 using System.Reflection;
+using UnityEngine.InputSystem;
+using OWML.Utils;
 
 namespace ScoutStreaming
 {
@@ -15,23 +17,19 @@ namespace ScoutStreaming
         private MethodInfo snapshotMethod;
         private void Start()
         {
-            ModHelper.Console.WriteLine("Skipping splash screen...");
-            var titleScreenAnimation = FindObjectOfType<TitleScreenAnimation>();
-            titleScreenAnimation.SetValue("_fadeDuration", 0);
-            titleScreenAnimation.SetValue("_gamepadSplash", false);
-            titleScreenAnimation.SetValue("_introPan", false);
-            titleScreenAnimation.Invoke("FadeInTitleLogo");
-            ModHelper.Console.WriteLine("Done!");
-
-   
             Instance = this;
 
             snapshotMethod = typeof(QuantumObject).GetMethod("OnProbeSnapshot",
-                      BindingFlags.NonPublic | BindingFlags.Instance); 
+                      BindingFlags.NonPublic | BindingFlags.Instance);
 
             ModHelper.HarmonyHelper.AddPostfix<ProbeLauncher>("TakeSnapshotWithCamera", typeof(ScoutPatch), "SnapshotPatch");
+            ModHelper.HarmonyHelper.AddPostfix<ProbeLauncher>("EquipTool", typeof(ScoutPatch), "EquipToolPatch");
+            ModHelper.HarmonyHelper.AddPostfix<ProbeLauncher>("LaunchProbe", typeof(ScoutPatch), "LaunchProbePatch");
+            ModHelper.HarmonyHelper.AddPostfix<ProbeLauncher>("RetrieveProbe", typeof(ScoutPatch), "RetrieveProbePatch");
+            ModHelper.HarmonyHelper.AddPostfix<SurveyorProbe>("OnAnchor", typeof(ScoutPatch), "OnAnchorPatch");
             ModHelper.HarmonyHelper.AddPostfix<SatelliteSnapshotController>("OnPressInteract", typeof(ScoutPatch), "HearthianSatteliteOnPatch");
             ModHelper.HarmonyHelper.AddPostfix<SatelliteSnapshotController>("TurnOffProjector", typeof(ScoutPatch), "HearthianSatteliteOffPatch");
+
             ModHelper.Events.Scenes.OnCompleteSceneChange += OnCompleteSceneChange;
         }
         private void Update()
@@ -51,28 +49,91 @@ namespace ScoutStreaming
                 quantumObjects = FindObjectsOfType<QuantumObject>();
                 OWML.Utils.TypeExtensions.SetValue(FindObjectOfType<ProbeLauncherUI>(), "s_takeSnapshotPrompt", new ScreenPrompt("Start Filming"));
             }
-
         }
+
+        private static void EquipToolPatch()
+        {
+            ProbeLauncher playerProbeLauncher = null;
+            foreach (var launcher in FindObjectsOfType<ProbeLauncher>())
+            {
+                if (launcher.GetName().ToString() == "Player")
+                {
+                    playerProbeLauncher = launcher;
+                }
+            }
+            playerProbeLauncher.SendMessage("TakeSnapshotWithCamera", playerProbeLauncher.GetValue<ProbeCamera>("_preLaunchCamera"));
+            EnableCamera(playerProbeLauncher.GetValue<ProbeCamera>("_preLaunchCamera"));
+        }
+
+        private static void LaunchProbePatch()
+        {
+            ProbeLauncher playerProbeLauncher = null;
+            foreach (var launcher in FindObjectsOfType<ProbeLauncher>())
+            {
+                if (launcher.GetName().ToString() == "Player")
+                {
+                    playerProbeLauncher = launcher;
+                }
+            }
+            playerProbeLauncher.SendMessage("TakeSnapshotWithCamera", playerProbeLauncher.GetValue<SurveyorProbe>("_activeProbe").GetForwardCamera());
+            EnableCamera(playerProbeLauncher.GetValue<SurveyorProbe>("_activeProbe").GetForwardCamera());
+        }
+
+        private static void RetrieveProbePatch()
+        {
+            Instance.Invoke("RetakeSnapshotAfterRetrieval", 0.5f);
+        }
+        private static void OnAnchorPatch()
+        {
+            ProbeLauncher playerProbeLauncher = null;
+            foreach (var launcher in FindObjectsOfType<ProbeLauncher>())
+            {
+                if (launcher.GetName().ToString() == "Player")
+                {
+                    playerProbeLauncher = launcher;
+                }
+            }
+            playerProbeLauncher.SendMessage("TakeSnapshotWithCamera", playerProbeLauncher.GetValue<SurveyorProbe>("_activeProbe").GetRotatingCamera());
+            EnableCamera(playerProbeLauncher.GetValue<SurveyorProbe>("_activeProbe").GetRotatingCamera());
+        }
+
         private static void SnapshotPatch(ProbeCamera camera)
         {
             Instance.probeCamera = camera;
-
             foreach (var probeCamera in FindObjectsOfType<ProbeCamera>())
             {
                 probeCamera.GetComponent<OWCamera>().enabled = false;
             }
-
-
-            camera.GetOWCamera().enabled = true;
-            Instance.ModHelper.Console.WriteLine(camera.GetOWCamera().gameObject.name + " on");
         }
 
+        private static void EnableCamera(ProbeCamera camera)
+        {
+            Instance.probeCamera = camera;
+            foreach (var probeCamera in FindObjectsOfType<ProbeCamera>())
+            {
+                probeCamera.GetComponent<OWCamera>().enabled = false;
+            }
+            camera.GetOWCamera().enabled = true;
+        }
+        private void RetakeSnapshotAfterRetrieval()
+        {
+            ProbeLauncher playerProbeLauncher = null;
+            foreach (var launcher in FindObjectsOfType<ProbeLauncher>())
+            {
+                if (launcher.GetName().ToString() == "Player")
+                {
+                    playerProbeLauncher = launcher;
+                }
+            }
+            playerProbeLauncher.SendMessage("TakeSnapshotWithCamera", playerProbeLauncher.GetValue<ProbeCamera>("_preLaunchCamera"));
+            EnableCamera(playerProbeLauncher.GetValue<ProbeCamera>("_preLaunchCamera"));
+        }
         private static void HearthianSatteliteOnPatch()
         {
             SatelliteSnapshotController museumProjector = FindObjectOfType<SatelliteSnapshotController>();
             var value = OWML.Utils.TypeExtensions.GetValue<OWCamera>(museumProjector, "_satelliteCamera");
             value.enabled = true;
-        }   
+        }
         private static void HearthianSatteliteOffPatch()
         {
             SatelliteSnapshotController museumProjector = FindObjectOfType<SatelliteSnapshotController>();
